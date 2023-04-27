e5f9bd65f2d5cf1d96864c36416d7e1340e5139b
SouthWind6510 <z1113270382@gmail.com>
2023-04-27 22:20:37 +0800

    调用jinrishici API

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # zsh-jinrishici
+每次打开终端随机显示一句古诗词，基于[今日诗词API](https://www.jinrishici.com/)。
+
+使用前需要先获取Token，在命令行中执行`curl https://v2.jinrishici.com/token` ，用响应中的data替换`~/zsh-jinrishici.plugin.zsh`文件中的`{Token}`。
---
+++ zsh-jinrishici.plugin.zsh
@@ -0,0 +1,11 @@
+if ! (( $+commands[curl] )); then
+    echo "jinrishici plugin needs curl to work" >&2
+    return
+fi
+
+if ! (( $+commands[jq] )); then
+    echo "jinrishici plugin needs jq to work" >&2
+    return
+fi
+
+curl --connect-timeout 0.2 -sL 'https://v2.jinrishici.com/sentence' -H 'X-User-Token: {TOKEN} | jq '.data.content, .data.origin.title, .data.origin.dynasty, .data.origin.author'
